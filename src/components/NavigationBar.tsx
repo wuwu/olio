8d7aed01ae25230b47a36ea1b8653f057fffb3e6
Florian Ewald <florian.ewald@accenture.com>
2024-11-20 09:25:08 +0100

    feat: order form

---
+++ src/components/NavigationBar.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+
+
+const NavigationBar: React.FC= () => {
+  return (
+    <div className="navbar bg-white_smoke-400 h-50 fixed top-0  border-b border-b-slate-500 z-50">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor">
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16" />
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+          <li><a href="/">Home</a></li>
+            <li>
+              <a>Parent</a>
+              <ul className="p-2">
+                <li><a>Submenu 1</a></li>
+                <li><a>Submenu 2</a></li>
+              </ul>
+            </li>
+            <li><a>Item 3</a></li>
+          </ul>
+        </div>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
+          <li><a href="/">Home</a></li>
+          <li>
+            <details>
+              <summary>Parent</summary>
+              <ul className="p-2">
+                <li><a>Submenu 1</a></li>
+                <li><a>Submenu 2</a></li>
+              </ul>
+            </details>
+          </li>
+          <li><a>Item 3</a></li>
+        </ul>
+      </div>
+      <div className="navbar-end">
+        <a className="btn bg-primary" href="checkout">Bestellen</a>
+      </div>
+    </div>
+  );
+};
+
+export default NavigationBar
